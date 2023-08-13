7b0f140b598bc127496d2bbc70a28fa5bb94ca0c
Vovashprot <vova.protasenko@gmail.com>
2023-08-13 14:41:01 +0300

    IntList() contains

--- List-master/demo/src/main/java/IntList/IntListImpl.java
+++ List-master/demo/src/main/java/IntList/IntListImpl.java
@@ -74,9 +74,21 @@ public class IntListImpl implements IntList {
 
     @Override
     public boolean contains(Integer item) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i].equals(item))
+        int min = 0;
+        int max = array.length - 1;
+
+        while (min <= max) {
+            int mid = (min + max) / 2;
+
+            if (item == array[mid]) {
                 return true;
+            }
+
+            if (item < array[mid]) {
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
         }
         return false;
     }
